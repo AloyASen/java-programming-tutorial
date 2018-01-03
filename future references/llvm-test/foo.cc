01acf3131e392ca9c90ec7fab317dbab02f5eae0
aloy aditya sen <aloysen951@gmail.com>
2018-01-03 17:42:32 +0530

    these pages are for the future developments of the future

--- future references/llvm-test/foo.cc
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-#include "llvm/ADT/STLExtras.h"
-int main(){
-std::cout << "remanning "<<std::endl;
-return 0;
-}
--- future references/llvm-test/modify.md
+++
@@ -1,6 +0,0 @@
-
-dated :: 25 11 2017
-
-this is a base program tested on ubuntu 64 bit arch
-
-the program is performing correctly 
